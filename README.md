e1fef35cf25d4d63abbce6867690757419bd0858
martinlaxenaire <martin.laxenaire@gmail.com>
2023-12-22 13:19:29 +0100

    - added a textures transformations test - added priority to gpu renderer dom element

--- README.md
+++ README.md
@@ -10,6 +10,70 @@ Although it can theoretically be used as a genuine 3D engine, its main purpose i
 
 The project was initially conceived as a WebGPU port of [curtains.js](https://github.com/martinlaxenaire/curtainsjs). It turned out to be a complete rewrite of the library instead, but with a very similar API.
 
+## Basic example
+
+### HTML
+
+```html
+<body>
+    <!-- div that will hold our WebGPU canvas -->
+    <div id="canvas"></div>
+</body>
+```
+
+### CSS
+
+```css
+body {
+    /* make the body fits our viewport */
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    margin: 0;
+    overflow: hidden;
+}
+
+#canvas {
+    /* make the canvas wrapper fits the viewport as well */
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+```
+
+### Javascript
+
+```javascript
+import { Curtains, Mesh } from 'gpu-curtains';
+
+window.addEventListener('DOMContentLoaded', async () => {
+  // set our main GPUCurtains instance
+  // it will handle everything we need
+  // a WebGPU device and a renderer with its scene,
+  // requestAnimationFrame, resize and scroll events...
+  const gpuCurtains = new GPUCurtains({
+    container: '#canvas'
+  })
+
+  // set the GPU device
+  // note this is asynchronous
+  await gpuCurtains.setDevice()
+
+  // create a cube mesh
+  const mesh = new Mesh(gpuCurtains, {
+    geometry: new BoxGeometry(),
+  })
+
+  // make it rotate
+  mesh.onRender(() => {
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.02
+  })
+})
+```
+
 ### Roadmap
 
 See the [roadmap](ROADMAP.md) for details on the current work in progress.
---
+++ tests/textures-transformations/main.js
@@ -0,0 +1,68 @@
+import { GPUCurtains, Plane } from '../../src'
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const systemSize = 50
+
+  // set up our WebGL context and append the canvas to our wrapper
+  const gpuCurtains = new GPUCurtains({
+    container: '#canvas',
+    pixelRatio: Math.min(1.5, window.devicePixelRatio), // limit pixel ratio for performance
+  })
+
+  await gpuCurtains.setDevice()
+
+  const vertexShader = /* wgsl */ `
+    struct VSOutput {
+      @builtin(position) position: vec4f,
+      @location(0) uv: vec2f,
+    };
+    
+    @vertex fn main(
+      attributes: Attributes,
+    ) -> VSOutput {
+      var vsOutput: VSOutput;
+
+      vsOutput.position = getOutputPosition(camera, matrices, attributes.position);
+      vsOutput.uv = getUVCover(attributes.uv, planeTextureMatrix);
+    
+      return vsOutput;
+    }
+  `
+
+  const fragmentShader = /* wgsl */ `
+    struct VSOutput {
+      @builtin(position) position: vec4f,
+      @location(0) uv: vec2f,
+    };
+    
+    @fragment fn main(fsInput: VSOutput) -> @location(0) vec4f {   
+      var color: vec4f = textureSample(planeTexture, defaultSampler, fsInput.uv);
+
+      return color;
+    }
+  `
+
+  const planeEls = document.querySelectorAll('.plane')
+
+  planeEls.forEach((planeEl, index) => {
+    const plane = new Plane(gpuCurtains, planeEl, {
+      shaders: {
+        vertex: {
+          code: vertexShader,
+        },
+        fragment: {
+          code: fragmentShader,
+        },
+      },
+    })
+
+    plane.textures[0].scale.set(1.5, 1.5, 1)
+
+    if (index % 2 === 1) {
+      plane.onRender(() => {
+        plane.rotation.z += 0.01
+        plane.textures[0].rotation.z -= 0.01
+      })
+    }
+  })
+})
