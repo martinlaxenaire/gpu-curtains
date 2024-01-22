4744d4c408130253f9b2f4439d3043c59e21a141
martinlaxenaire <martin.laxenaire@gmail.com>
2024-01-22 10:47:50 +0100

    - fixed render pass depth texture resizing + build

--- dist/gpu-curtains.js
+++ dist/gpu-curtains.js
@@ -7712,16 +7712,13 @@ class RenderPass {
    * Set our {@link depthTexture | depth texture}
    */
   createDepthTexture() {
-    if (this.options.depthTexture) {
-      this.depthTexture = this.options.depthTexture;
-      return;
-    }
     this.depthTexture = new RenderTexture(this.renderer, {
       label: this.options.label + " depth texture",
       name: "depthTexture",
       usage: "depthTexture",
       format: "depth24plus",
-      sampleCount: this.options.sampleCount
+      sampleCount: this.options.sampleCount,
+      ...this.options.depthTexture && { fromTexture: this.options.depthTexture }
     });
   }
   /**
