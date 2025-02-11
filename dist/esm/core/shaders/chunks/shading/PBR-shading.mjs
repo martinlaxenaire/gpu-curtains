cf50f4ffccf8ff4ffa5ce775db1983041c1d6b44
martinlaxenaire <martin.laxenaire@gmail.com>
2025-02-11 20:06:16 +0100

    - git was ignoring cases!

---
+++ dist/esm/core/shaders/chunks/shading/PBR-shading.mjs
@@ -0,0 +1,56 @@
+import { lambertUtils } from './lambert-shading.mjs';
+import { REIndirectSpecular } from '../fragment/head/RE-indirect-specular.mjs';
+import { getIBLTransmission } from '../fragment/head/get-IBL-transmission.mjs';
+import { getPBRDirect } from '../fragment/head/get-PBR-direct.mjs';
+import { getPBRShading } from '../fragment/body/get-PBR-shading.mjs';
+import { applyToneMapping } from '../fragment/body/apply-tone-mapping.mjs';
+
+const getPBR = ({
+  addUtils = true,
+  receiveShadows = false,
+  toneMapping,
+  useOcclusion = false,
+  environmentMap = null,
+  transmissionBackgroundTexture = null,
+  extensionsUsed = []
+} = {}) => (
+  /* wgsl */
+  `
+${addUtils ? lambertUtils : ""}
+${REIndirectSpecular}
+${getIBLTransmission}
+${getPBRDirect}
+
+fn getPBR(
+  normal: vec3f,
+  worldPosition: vec3f,
+  color: vec4f,
+  viewDirection: vec3f,
+  metallic: f32,
+  roughness: f32,
+  specularIntensity: f32,
+  specularColor: vec3f,
+  ior: f32,
+  transmission: f32,
+  dispersion: f32,
+  thickness: f32,
+  attenuationDistance: f32,
+  attenuationColor: vec3f,
+  ${useOcclusion ? "occlusion: f32," : ""}
+) -> vec4f {
+  ${!useOcclusion ? "let occlusion: f32 = 1.0;" : ""}
+  
+  var outputColor: vec4f = color;
+  
+  ${getPBRShading({ receiveShadows, environmentMap, transmissionBackgroundTexture, extensionsUsed })}
+  
+  outputColor = vec4(outgoingLight, outputColor.a);
+  
+  ${applyToneMapping({ toneMapping })}
+    
+  return outputColor;
+}
+`
+);
+
+export { getPBR };
---
+++ dist/esm/core/shaders/full/fragment/get-PBR-fragment-shader-code.mjs
@@ -0,0 +1,94 @@
+import { constants } from '../../chunks/utils/constants.mjs';
+import { common } from '../../chunks/utils/common.mjs';
+import { toneMappingUtils } from '../../chunks/utils/tone-mapping-utils.mjs';
+import { getLightsInfos } from '../../chunks/fragment/head/get-lights-infos.mjs';
+import { REIndirectDiffuse } from '../../chunks/fragment/head/RE-indirect-diffuse.mjs';
+import { REIndirectSpecular } from '../../chunks/fragment/head/RE-indirect-specular.mjs';
+import { getPBRDirect } from '../../chunks/fragment/head/get-PBR-direct.mjs';
+import { getIBLIndirect } from '../../chunks/fragment/head/get-IBL-indirect.mjs';
+import { getIBLTransmission } from '../../chunks/fragment/head/get-IBL-transmission.mjs';
+import { getPBRShading } from '../../chunks/fragment/body/get-PBR-shading.mjs';
+import { getFragmentInputStruct } from '../../chunks/fragment/head/get-fragment-input-struct.mjs';
+import { declareAttributesVars } from '../../chunks/fragment/body/declare-attributes-vars.mjs';
+import { declareMaterialVars } from '../../chunks/fragment/body/declare-material-vars.mjs';
+import { getBaseColor } from '../../chunks/fragment/body/get-base-color.mjs';
+import { getNormalTangentBitangent } from '../../chunks/fragment/body/get-normal-tangent-bitangent.mjs';
+import { getMetallicRoughness } from '../../chunks/fragment/body/get-metallic-roughness.mjs';
+import { getSpecular } from '../../chunks/fragment/body/get-specular.mjs';
+import { getTransmissionThickness } from '../../chunks/fragment/body/get-transmission-thickness.mjs';
+import { getEmissiveOcclusion } from '../../chunks/fragment/body/get-emissive-occlusion.mjs';
+import { applyToneMapping } from '../../chunks/fragment/body/apply-tone-mapping.mjs';
+import { patchAdditionalChunks } from '../../default-material-helpers.mjs';
+
+const getPBRFragmentShaderCode = ({
+  chunks = null,
+  toneMapping = "Khronos",
+  geometry,
+  additionalVaryings = [],
+  materialUniform = null,
+  materialUniformName = "material",
+  extensionsUsed = [],
+  receiveShadows = false,
+  baseColorTexture = null,
+  normalTexture = null,
+  emissiveTexture = null,
+  occlusionTexture = null,
+  metallicRoughnessTexture = null,
+  specularTexture = null,
+  specularFactorTexture = null,
+  specularColorTexture = null,
+  transmissionTexture = null,
+  thicknessTexture = null,
+  transmissionBackgroundTexture = null,
+  environmentMap = null
+}) => {
+  chunks = patchAdditionalChunks(chunks);
+  return (
+    /* wgsl */
+    `  
+${chunks.additionalHead}
+
+${constants}
+${common}
+${toneMappingUtils}
+${getLightsInfos}
+${REIndirectDiffuse}
+${REIndirectSpecular}
+${getPBRDirect}
+${getIBLIndirect}
+${getIBLTransmission}
+
+${getFragmentInputStruct({ geometry, additionalVaryings })}
+
+@fragment fn main(fsInput: FSInput) -> @location(0) vec4f {
+  var outputColor: vec4f = vec4();
+  
+  ${declareAttributesVars({ geometry, additionalVaryings })}
+  ${declareMaterialVars({ materialUniform, materialUniformName, shadingModel: "PBR", environmentMap })}
+  ${getBaseColor({ geometry, baseColorTexture })}
+  
+  // user defined preliminary contribution
+  ${chunks.preliminaryContribution}
+  
+  ${getNormalTangentBitangent({ geometry, normalTexture })}
+  ${getMetallicRoughness({ metallicRoughnessTexture })}
+  ${getSpecular({ specularTexture, specularFactorTexture, specularColorTexture })}
+  ${getTransmissionThickness({ transmissionTexture, thicknessTexture })}
+  ${getEmissiveOcclusion({ emissiveTexture, occlusionTexture })}
+  
+  // lights
+  ${getPBRShading({ receiveShadows, environmentMap, transmissionBackgroundTexture, extensionsUsed })}
+  
+  outputColor = vec4(outgoingLight, outputColor.a);
+  outputColor = vec4(outputColor.rgb + emissive, outputColor.a);
+  
+  // user defined additional contribution
+  ${chunks.additionalContribution}
+  
+  ${applyToneMapping({ toneMapping })}
+  return outputColor;
+}`
+  );
+};
+
+export { getPBRFragmentShaderCode };
