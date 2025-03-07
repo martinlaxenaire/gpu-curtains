4f3f6bd75cee728ecfe966fa6b2aa196f03ab660
martinlaxenaire <martin.laxenaire@gmail.com>
2025-03-07 08:47:09 +0100

    - fixed fullscreenplane samplers issue when no shaders passed

--- src/core/materials/Material.ts
+++ src/core/materials/Material.ts
@@ -642,10 +642,15 @@ export class Material {
 
     // is it used in our shaders?
     if (
-      (this.options.shaders.vertex && this.options.shaders.vertex.code.indexOf(sampler.name) !== -1) ||
-      (this.options.shaders.fragment &&
+      (this.options.shaders &&
+        this.options.shaders.vertex &&
+        this.options.shaders.vertex.code.indexOf(sampler.name) !== -1) ||
+      (this.options.shaders &&
+        this.options.shaders.fragment &&
         (this.options.shaders.fragment as ShaderOptions).code.indexOf(sampler.name) !== -1) ||
-      (this.options.shaders.compute && this.options.shaders.compute.code.indexOf(sampler.name) !== -1)
+      (this.options.shaders &&
+        this.options.shaders.compute &&
+        this.options.shaders.compute.code.indexOf(sampler.name) !== -1)
     ) {
       this.texturesBindGroup.addSampler(sampler)
     }
