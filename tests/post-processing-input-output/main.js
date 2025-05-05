0ff030f587fb701aaa56cc63cd28123a8ecdbb9f
martinlaxenaire <martin.laxenaire@gmail.com>
2025-05-05 11:22:47 +0200

    - improved input/output targets test

--- tests/post-processing-input-output/main.js
+++ tests/post-processing-input-output/main.js
@@ -1,6 +1,6 @@
 // Post processing input/output target tests
 // The goal is to render some objects into a separate input target (could be at a lower resolution)
-// we apply a post processing pass (here we just invert colors) to that input target content and render to another output target
+// we apply a post processing pass (grayscale) to that input target content and render to another output target
 // we then use two gaussian blur passes (vertical/horizontal) using the output target as input, and another blur target as output
 // we compose our final scene with a last post processing pass, where we blend the regular main buffer content with our blur output target
 window.addEventListener('load', async () => {
@@ -24,20 +24,23 @@ window.addEventListener('load', async () => {
   const renderer = new GPUCameraRenderer({
     deviceManager: gpuDeviceManager,
     container: document.querySelector('#canvas'),
-    pixelRatio: Math.min(1.5, window.devicePixelRatio),
+    pixelRatio: 1,
   })
 
-  // could be change later
+  const outputTargetRenderTextureName = 'grayscaleTexture'
+
+  // could be changed later
   const inputQuality = 0.5
 
-  const invertInputTarget = new RenderTarget(renderer, {
-    label: 'Invert colors input target',
+  // we'll need 3 render targets
+  const grayscaleInputTarget = new RenderTarget(renderer, {
+    label: 'Grayscale colors input target',
     qualityRatio: inputQuality,
   })
 
-  const invertOutputTarget = new RenderTarget(renderer, {
-    label: 'Invert colors output target',
-    renderTextureName: 'invertedTexture',
+  const grayscaleOutputTarget = new RenderTarget(renderer, {
+    label: 'Grayscale colors output target',
+    renderTextureName: outputTargetRenderTextureName,
     useDepth: false,
     sampleCount: 1,
   })
@@ -52,7 +55,7 @@ window.addEventListener('load', async () => {
   const cube1 = new Mesh(renderer, {
     label: 'Cube 1',
     geometry: new BoxGeometry(),
-    outputTarget: invertInputTarget,
+    outputTarget: grayscaleInputTarget, // render to input target
   })
 
   cube1.position.x = -3
@@ -74,11 +77,11 @@ window.addEventListener('load', async () => {
 
   // post pro chain
 
-  // first we will invert colors of cube1
-  const invertPass = new ShaderPass(renderer, {
+  // first we will convert colors of cube1 to grayscale
+  const bwPass = new ShaderPass(renderer, {
     label: 'Invert colors pass',
-    inputTarget: invertInputTarget, // use our invert input target as input
-    outputTarget: invertOutputTarget, // use our invert output target as output
+    inputTarget: grayscaleInputTarget, // use our grayscale input target as input
+    outputTarget: grayscaleOutputTarget, // use our grayscale output target as output
     renderTextureName: 'sceneInputTexture',
     targets: [
       {
@@ -104,7 +107,8 @@ window.addEventListener('load', async () => {
 
           @fragment fn main(fsInput: VSOutput) -> @location(0) vec4<f32> {
             let sceneSample = textureSample(sceneInputTexture, defaultSampler, fsInput.uv);
-            return vec4(1.0 - vec3(sceneSample.rgb), sceneSample.a);
+            let grayscale: vec3f = vec3(sceneSample.r * 0.3 + sceneSample.g * 0.59 + sceneSample.b * 0.11);
+            return vec4(grayscale, sceneSample.a);
           }
         `,
       },
@@ -112,7 +116,9 @@ window.addEventListener('load', async () => {
   })
 
   // then a gaussian blur in 2 passes,
-  // taking the inverted colors output target as input
+  // taking the grayscale colors output target as input
+
+  const gaussianSigma = 3
 
   const gaussianBlurPassFs = /* wgsl */ `
     struct VSOutput {
@@ -122,29 +128,39 @@ window.addEventListener('load', async () => {
 
     @fragment
     fn main(fsInput: VSOutput) -> @location(0) vec4f {
-        let offsets = array<i32, 13>(-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6);
-        let weights = array<f32, 13>(
-            0.00916, 0.01979, 0.03877, 0.07030, 0.11588,
-            0.16311, 0.18367,
-            0.16311, 0.11588, 0.07030, 0.03877, 0.01979, 0.00916
-          );
-
-        let textureSize = vec2f(textureDimensions(invertedTexture));
+        let textureSize = vec2f(textureDimensions(${outputTargetRenderTextureName}));
         let texelSize: vec2f = 1.0 / textureSize;
+
+        var totalWeight = 0.0;
+        var color = vec4f(0.0);
 
         let sigma: f32 = params.sigma;
         let kernelRadius = i32(ceil(sigma * 3.0));
-        var color = vec4f(0.0);
-        var totalWeight = 0.0;
 
         for (var i = -kernelRadius; i <= kernelRadius; i++) {
             let offset = f32(i);
             let weight = exp(-0.5 * (offset / sigma) * (offset / sigma));
             let sampleUv = fsInput.uv + params.direction * texelSize * offset;
 
-            color += textureSample(invertedTexture, defaultSampler, sampleUv) * weight;
+            color += textureSample(${outputTargetRenderTextureName}, defaultSampler, sampleUv) * weight;
             totalWeight += weight;
         }
+
+        // let offsets = array<i32, 13>(-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6);
+        // let weights = array<f32, 13>(
+        //     0.00916, 0.01979, 0.03877, 0.07030, 0.11588,
+        //     0.16311, 0.18367,
+        //     0.16311, 0.11588, 0.07030, 0.03877, 0.01979, 0.00916
+        //   );
+
+        // // let offsets = array<i32, 5>(-2, -1, 0, 1, 2);
+        // // let weights = array<f32, 5>(0.06136, 0.24477, 0.38774, 0.24477, 0.06136);
+
+        // for (var i = -6; i <= 6; i++) {
+        //     let sampleUv = fsInput.uv + params.direction * f32(offsets[i]) * texelSize;
+        //     color += textureSample(${outputTargetRenderTextureName}, defaultSampler, sampleUv) * weights[i];
+        //     totalWeight += weights[i];
+        // }
 
         return color / totalWeight;
     }`
@@ -152,10 +168,10 @@ window.addEventListener('load', async () => {
   // first pass
   const horizontalBlurPass = new ShaderPass(renderer, {
     label: 'Horizontal blur pass',
-    inputTarget: invertOutputTarget, // input is our invert output target
+    inputTarget: grayscaleOutputTarget, // input is our grayscale output target
     outputTarget: blurTarget, // output in a separate blur target
-    renderTextureName: 'invertedTexture', // we'll be using it in next pass, so name must match invert target renderTexture
-    copyOutputToRenderTexture: true, // copy the content of the pass to the invert target texture
+    renderTextureName: outputTargetRenderTextureName, // we'll be using it in next pass, so name must match grayscale target renderTexture
+    copyOutputToRenderTexture: true, // copy the content of the pass to the grayscale target texture
     uniforms: {
       params: {
         struct: {
@@ -165,7 +181,7 @@ window.addEventListener('load', async () => {
           },
           sigma: {
             type: 'f32',
-            value: 6,
+            value: gaussianSigma,
           },
         },
       },
@@ -179,9 +195,9 @@ window.addEventListener('load', async () => {
 
   const verticalBlurPass = new ShaderPass(renderer, {
     label: 'Vertical blur pass',
-    inputTarget: invertOutputTarget, // input is once again our invert output target
+    inputTarget: grayscaleOutputTarget, // input is once again our grayscale output target
     outputTarget: blurTarget, // output in a separate blur target
-    renderTextureName: 'invertedTexture', // we'll be using it in next pass, so name must match invertOutputTarget.renderTexture
+    renderTextureName: outputTargetRenderTextureName, // we'll be using it in next pass, so name must match grayscaleOutputTarget.renderTexture
     uniforms: {
       params: {
         struct: {
@@ -191,7 +207,7 @@ window.addEventListener('load', async () => {
           },
           sigma: {
             type: 'f32',
-            value: 6,
+            value: gaussianSigma,
           },
         },
       },
@@ -203,7 +219,7 @@ window.addEventListener('load', async () => {
     },
   })
 
-  // final pass to blend original unmodified cube (main scene) with our inverted blurred one
+  // final pass to blend original unmodified cube (main scene) with our grayscale blurred one
   const finalPass = new ShaderPass(renderer, {
     label: 'Final pass',
     // use our blur target texture
@@ -227,7 +243,7 @@ window.addEventListener('load', async () => {
     },
   })
 
-  console.log(invertOutputTarget, invertPass, finalPass)
+  console.log(grayscaleOutputTarget, bwPass, finalPass)
 
   const gui = new lil.GUI({
     title: 'Chaining input/output passes',
@@ -237,14 +253,22 @@ window.addEventListener('load', async () => {
     .add({ quality: inputQuality }, 'quality', 0.1, 1, 0.05)
     .name('Input target quality')
     .onChange((value) => {
-      invertInputTarget.setQualityRatio(value)
-      invertPass.renderTexture.copy(invertInputTarget.renderTexture)
+      grayscaleInputTarget.setQualityRatio(value)
+      grayscalePass.renderTexture.copy(grayscaleInputTarget.renderTexture)
     })
 
   gui
-    .add({ pixelRatio: renderer.pixelRatio }, 'pixelRatio', 0.5, 2, 0.1)
+    .add({ pixelRatio: renderer.pixelRatio }, 'pixelRatio', 0.5, window.devicePixelRatio, 0.1)
     .name('Renderer pixel ratio')
     .onChange((value) => {
       renderer.setPixelRatio(value)
+    })
+
+  gui
+    .add({ sigma: gaussianSigma }, 'sigma', 1, 6, 1)
+    .name('Blur strength')
+    .onChange((value) => {
+      horizontalBlurPass.uniforms.params.sigma.value = value
+      verticalBlurPass.uniforms.params.sigma.value = value
     })
 })
