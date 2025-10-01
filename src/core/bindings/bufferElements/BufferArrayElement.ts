10292306d60ced1e5b32fed99157191e0e680249
martinlaxenaire <martin.laxenaire@gmail.com>
2025-10-01 14:59:51 +0200

    fix: fixed uniform binding wrong alignment/stride for some array elements such as `f32`, `u32`, `i32`, `vec2f`...

--- src/core/bindings/bufferElements/BufferArrayElement.ts
+++ src/core/bindings/bufferElements/BufferArrayElement.ts
@@ -43,15 +43,26 @@ export class BufferArrayElement extends BufferElement {
    * Set the {@link core/bindings/bufferElements/BufferElement.BufferElementAlignment | alignment}.
    * To compute how arrays are packed, we get the second item alignment as well and use it to calculate the arrayStride between two array elements. Using the arrayStride and the total number of elements, we can easily get the end alignment position.
    * @param startOffset - Offset at which to start inserting the values in the {@link core/bindings/BufferBinding.BufferBinding#arrayBuffer | BufferBinding arrayBuffer}.
+   * @param minStride - Minimum stride to use for the values in the parent {@link core/bindings/BufferBinding.BufferBinding#arrayBuffer | BufferBinding arrayBuffer}. Uniform buffers array elements have a minimum stride of `16`.
    */
-  setAlignment(startOffset = 0) {
+  setAlignment(startOffset = 0, minStride = 0) {
+    if (minStride !== 0) {
+      startOffset = Math.ceil(startOffset / minStride) * minStride
+    }
+
     super.setAlignment(startOffset)
 
+    const endOffset = minStride !== 0 ? Math.ceil(this.endOffset / minStride) * minStride - 1 : this.endOffset
+
     // repeat for a second element to know how things are laid out
-    const nextAlignment = this.getElementAlignment(this.getPositionAtOffset(this.endOffset + 1))
+    const nextAlignment = this.getElementAlignment(this.getPositionAtOffset(endOffset + 1))
     this.arrayStride = this.getByteCountBetweenPositions(this.alignment.end, nextAlignment.end)
 
     this.alignment.end = this.getPositionAtOffset(this.endOffset + this.arrayStride * (this.numElements - 1))
+
+    if (minStride !== 0) {
+      this.alignment.end.byte = Math.max(this.alignment.end.byte, minStride - 1)
+    }
   }
 
   /**
