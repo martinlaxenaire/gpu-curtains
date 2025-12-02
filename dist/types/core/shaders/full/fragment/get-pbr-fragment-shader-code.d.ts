a35bb9ee58c08ed102de01b1d042bb5412686f9d
martinlaxenaire <martin.laxenaire@gmail.com>
2025-12-02 09:03:40 +0100

    - removed lowercase pbr* files to fix case collision

--- dist/types/core/shaders/full/fragment/get-pbr-fragment-shader-code.d.ts
+++
@@ -1,7 +0,0 @@
-import { PBRFragmentShaderInputParams } from './get-fragment-shader-code';
-/**
- * Build a PBR fragment shader using the provided options.
- * @param parameters - {@link PBRFragmentShaderInputParams} used to build the PBR fragment shader.
- * @returns - The PBR fragment shader generated based on the provided parameters.
- */
-export declare const getPBRFragmentShaderCode: ({ chunks, toneMapping, outputColorSpace, transmissiveInputColorSpace, transmissiveInputToneMapping, fragmentOutput, geometry, cullMode, flatShading, additionalVaryings, materialUniform, materialUniformName, extensionsUsed, receiveShadows, baseColorTexture, normalTexture, emissiveTexture, occlusionTexture, metallicRoughnessTexture, specularTexture, specularFactorTexture, specularColorTexture, transmissionTexture, thicknessTexture, sheenTexture, sheenColorTexture, sheenRoughnessTexture, anisotropyTexture, clearcoatTexture, clearcoatRoughnessTexture, clearcoatNormalTexture, iridescenceTexture, iridescenceFactorTexture, iridescenceThicknessTexture, diffuseTransmissionTexture, diffuseTransmissionFactorTexture, diffuseTransmissionColorTexture, transmissionBackgroundTexture, environmentMap, }: PBRFragmentShaderInputParams) => string;
